c900362d0f7cab108f7f61ecec0e81035f7f4daa
Jordan Irwin <antumdeluge@gmail.com>
2019-06-21 08:30:49 +0000

    Convert spaces to tabs in source

--- src/bin2header.cpp
+++ src/bin2header.cpp
@@ -14,106 +14,106 @@ const string version = "0.1.1";
 
 string GetFileName(string f) {
 #if defined (__WIN32__)
-    int split = f.find_last_of('\\');
+	int split = f.find_last_of('\\');
 #else
-    int split = f.find_last_of('/');
+	int split = f.find_last_of('/');
 #endif
-    f.erase(0, split + 1);
-    return f;
+	f.erase(0, split + 1);
+	return f;
 }
 
 int PrintUsage(string exe="bin2header") {
-    cout << "\nbin2header version " << version << endl;
-    cout << "2017 Jordan Irwin <antumdeluge@gmail.com>\n\n\tUsage:\t" << exe << " <file>\n" << endl;
-    return 1;
+	cout << "\nbin2header version " << version << endl;
+	cout << "2017 Jordan Irwin <antumdeluge@gmail.com>\n\n\tUsage:\t" << exe << " <file>\n" << endl;
+	return 1;
 }
 
 int main(int argc, char** argv) {
-    string executable = argv[0];
-    executable = GetFileName(executable);
+	string executable = argv[0];
+	executable = GetFileName(executable);
 
-    if (argc < 2) {
-        return PrintUsage(executable);
-    }
+	if (argc < 2) {
+		return PrintUsage(executable);
+	}
 
-    FILE* test;
-    test = fopen(argv[1], "r");
-    if (!test) {
-        cout << "\nFile: \"" << argv[1] << "\" does not exist\n";
-        return PrintUsage(executable);
-    }
-    fclose(test);
+	FILE* test;
+	test = fopen(argv[1], "r");
+	if (!test) {
+		cout << "\nFile: \"" << argv[1] << "\" does not exist\n";
+		return PrintUsage(executable);
+	}
+	fclose(test);
 
-    /* Get Filenames and Path */
-    string path = argv[1];
-    string filename = GetFileName(path);
-    string hname = filename;
+	/* Get Filenames and Path */
+	string path = argv[1];
+	string filename = GetFileName(path);
+	string hname = filename;
 
-    /* START Remove Unwanted Characters */
-    int hname_len = hname.length();
-    char badchars[6] = {'\\', '+', '-', '*', ' '};
-    int current = 0;
-    int x = 0;
-    for (current; current < hname_len; current++) {
-        x = 0;
-        for (x; x < len(badchars); x++) {
-            if ((hname[current] == badchars[x]) or (hname[current] == '.'))
-                hname.replace(current, 1, "_");
-            if (filename[current] == badchars[x])
-                filename.replace(current, 1, "_");
-        }
-    }
-    /* END Remove Unwanted Characters */
+	/* START Remove Unwanted Characters */
+	int hname_len = hname.length();
+	char badchars[6] = {'\\', '+', '-', '*', ' '};
+	int current = 0;
+	int x = 0;
+	for (current; current < hname_len; current++) {
+		x = 0;
+		for (x; x < len(badchars); x++) {
+			if ((hname[current] == badchars[x]) or (hname[current] == '.'))
+				hname.replace(current, 1, "_");
+			if (filename[current] == badchars[x])
+				filename.replace(current, 1, "_");
+		}
+	}
+	/* END Remove Unwanted Characters */
 
-    /* START Uppercase Name for Header */
-    filename += ".h";
+	/* START Uppercase Name for Header */
+	filename += ".h";
 
-    char hname_upper[hname_len + 2];
-    current = 0;
-    for (current; current < len(hname_upper); current++) {
-        hname_upper[current] = hname[current];
-        hname_upper[current] = toupper(hname_upper[current]);
-    }
+	char hname_upper[hname_len + 2];
+	current = 0;
+	for (current; current < len(hname_upper); current++) {
+		hname_upper[current] = hname[current];
+		hname_upper[current] = toupper(hname_upper[current]);
+	}
 
-    string name_upper_h = hname_upper;
-    name_upper_h.append("_H");
-    /* END Uppercase Name for Header */
+	string name_upper_h = hname_upper;
+	name_upper_h.append("_H");
+	/* END Uppercase Name for Header */
 
-    /* START Read Data In */
-    ifstream infile;
-    infile.open(path.c_str(), ifstream::binary);
+	/* START Read Data In */
+	ifstream infile;
+	infile.open(path.c_str(), ifstream::binary);
 
-    int data_length;
-    infile.seekg(0, ifstream::end);
-    data_length = infile.tellg();
-    infile.seekg(0, ifstream::beg);
+	int data_length;
+	infile.seekg(0, ifstream::end);
+	data_length = infile.tellg();
+	infile.seekg(0, ifstream::beg);
 
-    char data[data_length];
+	char data[data_length];
 
-    infile.read(data, data_length);
-    infile.close();
-    /* END Read Data In */
+	infile.read(data, data_length);
+	infile.close();
+	/* END Read Data In */
 
-    /* START Read Data Out to Header */
-    ofstream outfile(filename.c_str());
-    outfile << "#ifndef " << name_upper_h.c_str() << "\n#define " << name_upper_h.c_str() << "\n\nstatic const unsigned char " << hname << "[] = {\n";
+	/* START Read Data Out to Header */
+	ofstream outfile(filename.c_str());
+	outfile << "#ifndef " << name_upper_h.c_str() << "\n#define " << name_upper_h.c_str() << "\n\nstatic const unsigned char " << hname << "[] = {\n";
 
-    current = 0;
-    for (current; current < data_length; current++) {
-        if ((current % 12) == 0) outfile << "    ";
+	current = 0;
+	for (current; current < data_length; current++) {
+		if ((current % 12) == 0) outfile << "    ";
 
-        stringstream ss;
-        ss << "0x" << hex << setw(2) << setfill('0') << (int) (unsigned char) data[current];
-        outfile << ss.str();
+		stringstream ss;
+		ss << "0x" << hex << setw(2) << setfill('0') << (int) (unsigned char) data[current];
+		outfile << ss.str();
 
-        if ((current + 1) < data_length) outfile << ", ";
+		if ((current + 1) < data_length) outfile << ", ";
 
-        if ((current % 12) == 11) outfile << "\n";
-    }
+		if ((current % 12) == 11) outfile << "\n";
+	}
 
-    outfile << "\n};\n\n#endif\n";
-    outfile.close();
-    /* END Read Data Out to Header */
+	outfile << "\n};\n\n#endif\n";
+	outfile.close();
+	/* END Read Data Out to Header */
 
-    return 0;
+	return 0;
 }
--- src/bin2header.py
+++ src/bin2header.py
@@ -4,70 +4,70 @@
 import sys, os, array
 
 def PrintUsage():
-    executable = os.path.basename(__file__)
-    print u'\nbin2header version 0.1.1 (python)\n2017 Jordan Irwin <antumdeluge@gmail.com>\n\n\tUsage:\t{} file\n'.format(executable)
+	executable = os.path.basename(__file__)
+	print u'\nbin2header version 0.1.1 (python)\n2017 Jordan Irwin <antumdeluge@gmail.com>\n\n\tUsage:\t{} file\n'.format(executable)
 
 def Bin2Header(argv):
-    if not os.path.isfile(argv[1]):
-        print u'\nFile "{}" does not exist'.format(argv[1])
-        PrintUsage()
-        sys.exit(1)
-    
-    ### Get Filenames and Path ###
-    path = argv[1]
-    filename = list(os.path.basename(path))
-    hname = list(filename)
-    
-    ### Remove Unwanted Characters ###
-    badchars = (u'\\', u'+', u'-', u'*', u' ')
-    for x in range(len(hname)):
-        if hname[x] in badchars or hname[x] == u'.':
-            hname[x] = '_'
-        if filename[x] in badchars:
-            filename[x] = '_'
-    
-    filename = u''.join(filename)
-    filename += u'.h'
-    hname = u''.join(hname)
-    
-    ### Uppercase Name for Header ###
-    hname_upper = hname.upper()
-    hname_upper += u'_H'
-    
-    ### Read Data In ###
-    data = array.array('B', open(path, 'rb').read())
-    
-    ### START Read Data Out to Header ###
-    outfile = open(filename, 'w')
-    
-    text = u'#ifndef {}\n#define {}\n\nstatic const unsigned char {}[] = {}\n'.format(hname_upper, hname_upper, hname, u'{')
-    
-    current = 0
-    data_length = len(data)
-    for byte in data:
-        if (current % 12) == 0:
-            text += '    '
-        text += u'0x%02x' % byte
-    
-        if (current + 1) < data_length:
-            text += ', '
-        if (current % 12) == 11:
-            text += u'\n'
-        
-        current += 1
-    
-    text += u'\n};\n\n#endif\n'
-    
-    outfile.write(text)
-    outfile.close()
-    ### END Read Data Out to Header ###
-    
-    return 0
+	if not os.path.isfile(argv[1]):
+		print u'\nFile "{}" does not exist'.format(argv[1])
+		PrintUsage()
+		sys.exit(1)
+	
+	### Get Filenames and Path ###
+	path = argv[1]
+	filename = list(os.path.basename(path))
+	hname = list(filename)
+	
+	### Remove Unwanted Characters ###
+	badchars = (u'\\', u'+', u'-', u'*', u' ')
+	for x in range(len(hname)):
+		if hname[x] in badchars or hname[x] == u'.':
+			hname[x] = '_'
+		if filename[x] in badchars:
+			filename[x] = '_'
+	
+	filename = u''.join(filename)
+	filename += u'.h'
+	hname = u''.join(hname)
+	
+	### Uppercase Name for Header ###
+	hname_upper = hname.upper()
+	hname_upper += u'_H'
+	
+	### Read Data In ###
+	data = array.array('B', open(path, 'rb').read())
+	
+	### START Read Data Out to Header ###
+	outfile = open(filename, 'w')
+	
+	text = u'#ifndef {}\n#define {}\n\nstatic const unsigned char {}[] = {}\n'.format(hname_upper, hname_upper, hname, u'{')
+	
+	current = 0
+	data_length = len(data)
+	for byte in data:
+		if (current % 12) == 0:
+			text += '    '
+		text += u'0x%02x' % byte
+	
+		if (current + 1) < data_length:
+			text += ', '
+		if (current % 12) == 11:
+			text += u'\n'
+		
+		current += 1
+	
+	text += u'\n};\n\n#endif\n'
+	
+	outfile.write(text)
+	outfile.close()
+	### END Read Data Out to Header ###
+	
+	return 0
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        PrintUsage()
-        sys.exit(1)
-    
-    Bin2Header(sys.argv)
+	if len(sys.argv) < 2:
+		PrintUsage()
+		sys.exit(1)
+	
+	Bin2Header(sys.argv)
