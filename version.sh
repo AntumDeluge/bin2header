64555a5d223e56692c19ae4ecc2295dbd1c01c1f
Jordan Irwin <antumdeluge@gmail.com>
2019-09-10 04:36:43 -0700

    Keep versions segregated for C++ & Python implementations

--- version.sh
+++ version.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 VER=0.1.2
+PYVER=${VER}
 
 DIR_ROOT="$(dirname "$0")"
 cd "${DIR_ROOT}"
 DIR_ROOT="$(pwd)"
 
 sed -i -e "s|VERSION=.*$|VERSION=${VER}|" "${DIR_ROOT}/dist.sh"
-sed -i -e "s|	VERSION .*$|	VERSION ${VER}|" "${DIR_ROOT}/CMakeLists.txt"
-sed -i -e "s|^version =.*$|version = '${VER}'|" "${DIR_ROOT}/src/bin2header.py"
+sed -i -e "s|	VERSION .*$|	VERSION ${VER}|" -e "s|set(PROJECT_PY_VERSION.*)|set(PROJECT_PY_VERSION ${PYVER})|" "${DIR_ROOT}/CMakeLists.txt"
+sed -i -e "s|^version =.*$|version = '${PYVER}'|" "${DIR_ROOT}/src/bin2header.py"
 sed -i -e "s|version =.*;|version = \"${VER}\";|" "${DIR_ROOT}/src/bin2header.cpp"
