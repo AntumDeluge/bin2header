7eea59504f8744ecf56325ec1f4b62a3fbeb26ae
Jordan Irwin <antumdeluge@gmail.com>
2019-09-11 15:35:13 -0700

    Use version.sh script to update version in man file

--- version.sh
+++ version.sh
@@ -12,3 +12,4 @@ sed -i -e "s|^version =.*$|version = '${PYVERSION}'|" "${DIR_ROOT}/src/bin2heade
 sed -i -e "s|version =.*;|version = \"${VERSION}\";|" "${DIR_ROOT}/src/bin2header.cpp"
 sed -i -e "s|Latest release: \[v.*\]\(.*\)/tag/v.*)|Latest release: \[v${VERSION}\]\1/tag/v${VERSION})|" "${DIR_ROOT}/README.md"
 sed -i -e "s|Latest Python release: \[v.*\]\(.*\)/tag/v.*)|Latest Python release: \[v${PYVERSION}\]\1/tag/v${PYVERSION})|" "${DIR_ROOT}/README.md"
+sed -i -e "s|\"bin2header-.*\"|\"bin2header-${VERSION}\"|" "${DIR_ROOT}/man/bin2header.1"
