e8ef226561088d19da59f9c9defe6f0cae4b222c
Jordan Irwin <antumdeluge@gmail.com>
2022-08-06 05:34:57 -0700

    Add more output comparison tests

---
+++ tests/orig/flower.truncate.h
@@ -0,0 +1,8 @@
+#ifndef FLOWER_PNG_H
+#define FLOWER_PNG_H
+
+static const unsigned char flower_png[] = {
+	0x47, 0x0d, 0x0a, 0x1a, 0x0a
+};
+
+#endif /* FLOWER_PNG_H */
--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,10 +4,30 @@ cd "$(dirname $0)"
 
 mkdir "new/"
 
-./bin2header -o "new/flower.default.h" "flower.png"
-diff -q "orig/flower.default.h" "new/flower.default.h"
-res=$?
+compare() {
+	diff -q "orig/flower.$1.h" "new/flower.$1.h"
+	if test $? -gt 0; then
+		exit 1
+	fi
+}
 
-if test ${res} -gt 0; then
-	exit 1
-fi
+./bin2header -o "new/flower.default.h" "flower.png"
+compare "default"
+
+./bin2header -f 3 -l 5 -o "new/flower.truncate.h" "flower.png"
+compare "truncate"
+
+./bin2header -p 16 -o "new/flower.16.h" "flower.png"
+compare "16"
+
+./bin2header -p 32 -o "new/flower.32.h" "flower.png"
+compare "32"
+
+./bin2header -p 32 -e -o "new/flower.32be.h" "flower.png"
+compare "32be"
+
+./bin2header -n "rock" -o "new/flower.rock.h" "flower.png"
+compare "rock"
+
+./bin2header -c -o "new/flower.comment.h" "flower.png"
+compare "comment"
