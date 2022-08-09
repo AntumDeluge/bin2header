4aecf8481a7c2007bc6eb057452e87decf96877f
Jordan Irwin <antumdeluge@gmail.com>
2022-08-08 18:09:38 -0700

    Python: replace single quoted strings with double quotes

--- src/bin2header.py
+++ src/bin2header.py
@@ -9,20 +9,20 @@ import array, errno, os, sys, traceback
 
 
 if sys.version_info.major < 3:
-	print('\nERROR: Python ' + str(sys.version_info.major) + ' not supported. Please upgrade to Python 3.\n')
+	print("\nERROR: Python " + str(sys.version_info.major) + " not supported. Please upgrade to Python 3.\n")
 	sys.exit(2)
 
 
-__WIN32__ = 'windows' in str(os.getenv('OS')).lower();
+__WIN32__ = "windows" in str(os.getenv("OS")).lower();
 
 appname = "Binary to Header"
-version = '0.1.2'
+version = "0.1.2"
 executable = os.path.basename(__file__)
 
-symbolcp = '©'
+symbolcp = "©"
 if __WIN32__:
 	# some characters don't display correctly in Windows console
-	symbolcp = '(C)'
+	symbolcp = "(C)"
 
 
 # options configured from command parameters
@@ -182,19 +182,19 @@ def setOpt(key, value):
 #      Path formatted with native directory/node delimeters.
 def normalizePath(path):
 	new_path = path
-	to_replace = '\\'
-	replace_with = '/'
+	to_replace = "\\"
+	replace_with = "/"
 
 	if __WIN32__:
-		to_replace = '/'
-		replace_with = '\\'
+		to_replace = "/"
+		replace_with = "\\"
 
 	new_path = new_path.replace(to_replace, replace_with)
 
 	if __WIN32__:
 		# MSYS2/MinGW paths
-		if new_path.lower().startswith('\\c\\'):
-			new_path = 'C:{}'.format(new_path[2:])
+		if new_path.lower().startswith("\\c\\"):
+			new_path = "C:{}".format(new_path[2:])
 
 	return new_path;
 
@@ -209,8 +209,8 @@ def getBaseName(path):
 	base_name = os.path.basename(path)
 
 	# MSYS versions of Python appear to not understand Windows paths
-	if __WIN32__ and '\\' in base_name:
-		base_name = base_name.split('\\')[-1]
+	if __WIN32__ and "\\" in base_name:
+		base_name = base_name.split("\\")[-1]
 
 	return base_name
 
@@ -226,7 +226,7 @@ def getDirName(path):
 
 	# MSYS versions of Python appear to not understand Windows paths
 	if not dir_name and __WIN32__:
-		dir_name = '\\'.join(f.split('\\')[:-1])
+		dir_name = "\\".join(f.split("\\")[:-1])
 
 	return dir_name
 
@@ -250,7 +250,7 @@ def main(argv):
 
 	# check if file exists
 	if not os.path.isfile(source_file):
-		print('\nFile "{}" does not exist'.format(source_file))
+		print("\nFile \"{}\" does not exist".format(source_file))
 		printUsage()
 		return 1
 
@@ -260,23 +260,23 @@ def main(argv):
 	target_dir = getDirName(source_file)
 
 	# remove unwanted characters
-	badchars = ('\\', '+', '-', '*', ' ')
+	badchars = ("\\", "+", "-", "*", " ")
 	for x in range(len(hname)):
-		if hname[x] in badchars or hname[x] == '.':
-			hname[x] = '_'
+		if hname[x] in badchars or hname[x] == ".":
+			hname[x] = "_"
 		if filename[x] in badchars:
-			filename[x] = '_'
+			filename[x] = "_"
 
-	filename = ''.join(filename)
-	hname = ''.join(hname)
-	target_file = os.path.join(target_dir, filename) + '.h'
+	filename = "".join(filename)
+	hname = "".join(hname)
+	target_file = os.path.join(target_dir, filename) + ".h"
 
 	# uppercase name for header
 	hname_upper = hname.upper()
-	hname_upper += '_H'
+	hname_upper += "_H"
 
 	# read data in
-	data = array.array('B', open(source_file, 'rb').read())
+	data = array.array("B", open(source_file, "rb").read())
 
 	# *** START: write data *** #
 
@@ -285,44 +285,44 @@ def main(argv):
 	store_vector = True
 
 	# currently only support LF line endings output
-	outfile = open(target_file, 'w', newline='\n')
+	outfile = open(target_file, "w", newline="\n")
 
-	text = '#ifndef {0}\n#define {0}\n'.format(hname_upper)
+	text = "#ifndef {0}\n#define {0}\n".format(hname_upper)
 	if store_vector:
-		text += '\n#ifdef __cplusplus\n#include <vector>\n#endif\n'
-	text += '\nstatic const unsigned char {}[] = {{\n'.format(hname)
+		text += "\n#ifdef __cplusplus\n#include <vector>\n#endif\n"
+	text += "\nstatic const unsigned char {}[] = {{\n".format(hname)
 
 	current = 0
 	data_length = len(data)
 	for byte in data:
 		if (current % 12) == 0:
-			text += '    '
-		text += '0x%02x' % byte
+			text += "    "
+		text += "0x%02x" % byte
 
 		if (current + 1) < data_length:
-			text += ', '
+			text += ", "
 		if (current % 12) == 11:
-			text += '\n'
+			text += "\n"
 
 		current += 1
 
-	text += '\n};\n'
+	text += "\n};\n"
 	if store_vector:
-		text += '\n#ifdef __cplusplus\nstatic const std::vector<char> ' \
-		+ hname + '_v(' + hname + ', ' + hname + ' + sizeof(' + hname \
-		+ '));\n#endif\n'
-	text +='\n#endif /* {} */\n'.format(hname_upper)
+		text += "\n#ifdef __cplusplus\nstatic const std::vector<char> " \
+		+ hname + "_v(" + hname + ", " + hname + " + sizeof(" + hname \
+		+ "));\n#endif\n"
+	text +="\n#endif /* {} */\n".format(hname_upper)
 
 	outfile.write(text)
 	outfile.close()
 
 	# *** END: write data *** #
 
-	print('Exported to: {}'.format(target_file))
+	print("Exported to: {}".format(target_file))
 
 	return 0
 
 
 # program entry point.
-if __name__ == '__main__':
+if __name__ == "__main__":
 	sys.exit(main(sys.argv))
