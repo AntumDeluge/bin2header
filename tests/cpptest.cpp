1274f8ab38e30eb73bfa250d41e5f52c30911f98
Jordan Irwin <antumdeluge@gmail.com>
2022-08-10 15:26:39 -0700

    Add files for running misc. C++ & Python tests

---
+++ tests/cpptest.cpp
@@ -0,0 +1,12 @@
+
+// for running misc. C++ code tests
+
+#include <iostream>
+using namespace std;
+
+
+int main(int argc, char** argv) {
+	cout << "No tests found!" << endl;
+
+	exit(0);
+}
---
+++ tests/pytest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# for running misc. Python tests
+
+import sys
+
+
+def main(argv):
+	argc = len(argv)
+
+	print("No tests found!")
+
+	return 0
+
+
+if __name__ == "__main__":
+	sys.exit(main(list(sys.argv)))
